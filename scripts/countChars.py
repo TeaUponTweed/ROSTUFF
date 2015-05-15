54935be447126d1d082bdb58943600a92b764910
Troy A. Sornson <troyasornson37@gmail.com>
2015-05-15 12:20:29 -0600

    initial commit

---
+++ scripts/countChars.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from beginner_tutorials.srv import *
+import rospy
+
+def handle_add_two_ints(req):
+    print "Returning number of characters in " +
+    return AddTwoIntsResponse(req.a + req.b)
+
+def add_two_ints_server():
+    rospy.init_node('add_two_ints_server')
+    s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
+    print "Ready to add two ints."
+    rospy.spin()
+
+if __name__ == "__main__":
+    add_two_ints_server()
